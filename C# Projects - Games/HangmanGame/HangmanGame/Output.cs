b4b4655d8370fa7392010e9f184853aeceb51e51
Rado <sitcom93@abv.bg>
2024-10-13 19:44:36 +0300

    Initial commit

---
+++ C# Projects - Games/HangmanGame/HangmanGame/Output.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    public class Output
+    {
+        public static void MaxOut()
+        {
+            Console.WriteLine(@"______________");
+            Console.WriteLine(@"|      |     |");
+            Console.WriteLine(@"|     ( )    |");
+            Console.WriteLine(@"|     /|\    |");
+            Console.WriteLine(@"|    / | \   |");
+            Console.WriteLine(@"|     /|\    |");
+            Console.WriteLine(@"|    /   \   |");
+            Console.WriteLine(@"|            |");
+            Console.WriteLine(@"|____________|");
+            Console.WriteLine(@"|____________|");
+
+        }
+
+
+    }
+}
---
+++ C# Projects - Games/HangmanGame/HangmanGame/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HangmanGame;
+
+public class Program
+{
+    enum Words
+    {
+        apple, banana, cherry, watermelon
+    }
+
+    public static string GetRandomWord()
+    {
+        Random randomWord = new Random();
+        List<Words> word = Enum.GetValues(typeof(Words)).Cast<Words>().ToList();
+        int randomIndex = randomWord.Next(word.Count);
+        string wordForGame = word[randomIndex].ToString();
+        return wordForGame;
+    }
+
+    public static void GameOn()
+    {
+        string word = GetRandomWord();
+        int maxTries = 6;
+        int countTries = 0;
+        bool guestTheWord = false;
+
+        char[] charWords = word.ToArray();
+        char[] emptyCharArray = new char[charWords.Length];
+     
+        for(int i=0; i< emptyCharArray.Length; i++) emptyCharArray[i]='-';
+
+        int attempsToWin = charWords.Length;
+        int countToWin = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Let's Play a game of Hangman:");
+            Console.WriteLine("Enter a symbol:");
+            char guestSymbol = char.Parse(Console.ReadLine());
+
+            if (charWords.Contains(guestSymbol))
+            {
+                int indexAt = charWords.ToList().IndexOf(guestSymbol);
+                emptyCharArray[indexAt] = guestSymbol;
+                charWords[indexAt]='-';
+                Console.WriteLine($"Congratulation you have found the letter {guestSymbol}");
+                Console.WriteLine(string.Join("",emptyCharArray));
+                countToWin++;
+
+            }
+            else countTries++;
+
+            if (maxTries == countTries)
+            {
+                Output.MaxOut();
+                break;
+            }
+
+
+            if (attempsToWin== countToWin)
+            {
+                guestTheWord = true;
+                break;
+            }
+        }
+
+
+
+    }
+    public static void Main()
+    {
+        GameOn();
+    }
+}
