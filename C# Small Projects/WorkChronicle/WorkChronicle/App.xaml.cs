25cc12c63b4ef7f64704d4aa8adb8d8a0880991c
Rado <sitcom93@abv.bg>
2024-12-18 21:02:45 +0200

    Uploading a small project

---
+++ C# Small Projects/WorkChronicle/WorkChronicle/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkChronicle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
